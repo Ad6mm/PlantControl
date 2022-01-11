67893f6d778b90f889db35486a1fdb0d5903bb2e
Adam <adam_duszak@yahoo.pl>
2022-01-11 20:16:35 +0100

    List view TODO: custom ArrayAdapter https://guides.codepath.com/android/Using-an-ArrayAdapter-with-ListView

--- app/src/main/java/com/example/plantcontrol/MainActivity.java
+++ app/src/main/java/com/example/plantcontrol/MainActivity.java
@@ -7,9 +7,20 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Gravity;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.plantcontrol.data.Plant;
+
+import java.util.ArrayList;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,12 +28,21 @@ public class MainActivity extends AppCompatActivity {
     public static String USER_NAME = "userName";
     SharedPreferences sharedPref;
 
+    private ArrayList<Plant> items;
+    private ArrayAdapter<Plant> itemsAdapter;
+
+    ListView listView;
+    Button addPlantButton;
+    TextView userNameTextView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        TextView userNameTextView = findViewById(R.id.welcomeTextView);
+        userNameTextView = findViewById(R.id.welcomeTextView);
+        listView = findViewById(R.id.listView);
+        addPlantButton = findViewById(R.id.addPlantButton);
 
         sharedPref = getApplicationContext().getSharedPreferences(SP_NAME, Context.MODE_PRIVATE);
         String userName = sharedPref.getString("userName", null);
@@ -34,6 +54,17 @@ public class MainActivity extends AppCompatActivity {
             userNameTextView.setText(userNameTextView.getText().toString() + " " + userName + "!");
         }
 
+        addPlantButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                addItem(v);
+            }
+        });
+
+        items = new ArrayList<>();
+        itemsAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, items);
+        listView.setAdapter(itemsAdapter);
+        setUpListViewListener();
 
 
 
@@ -50,5 +81,26 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(welcomeViewIntent);
             }
         });*/
+    }
+
+    private void setUpListViewListener() {
+        listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+                Context context = getApplicationContext();
+                Toast toast =  Toast.makeText(context, "Item removed!", Toast.LENGTH_LONG);
+                toast.setGravity(Gravity.CENTER_HORIZONTAL, 0, 0);
+                toast.show();
+
+                items.remove(position);
+                itemsAdapter.notifyDataSetChanged();
+                return true;
+            }
+        });
+    }
+
+    private void addItem(View v) {
+        Plant plant = new Plant("Kwiatek", true, 3, new Date());
+        itemsAdapter.add(plant);
     }
 }
